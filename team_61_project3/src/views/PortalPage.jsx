cbfa987742ef63b82405cdd559331bd4a78634e7
Eli Goodrich <erg2598@tamu.edu>
2026-03-27 11:28:16 -0500

    updated portal

--- team_61_project3/src/views/PortalPage.jsx
+++ team_61_project3/src/views/PortalPage.jsx
@@ -2,7 +2,11 @@ function PortalPage() {
   return (
     <div>
       <h1>Home</h1>
-      <p>Welcome to the home page!</p>
+      <h2>Select the desired portal using the buttons below:</h2>
+      <button class = 'managerbtn'> Manager Portal</button>
+      <button class = 'cashierbtn'> Cashier Portal</button>
+      <button class = 'menubtn'> Display Menu</button>
+      <button class = 'kioskbtn'> Customer Kiosk Portal</button>
     </div>
   );
 }
