82ed663dab86f6eb887ff91bcf973337929f5e01
Nilay Alwar <npalwar@tamu.edu>
2026-03-27 11:16:11 -0500

    Added Kiosk.jsx

---
+++ team_61_project3/src/Kiosk.jsx
@@ -0,0 +1,11 @@
+function Kiosk() {
+    return(
+        <>
+            <h1>Kiosk</h1>
+            <button>Order Menu Item</button>
+            <button>Order Custom Item</button>
+        </>
+    )
+}
+
+export default Kiosk
