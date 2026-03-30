b545bcc36d7b3d4f4b05878ce3852d341a5cb907
Eli Goodrich <erg2598@tamu.edu>
2026-03-29 19:31:22 -0500

    created manager portal

--- team_61_project3/src/views/ManagerPortal.jsx
+++ team_61_project3/src/views/ManagerPortal.jsx
@@ -1,7 +1,16 @@
+import './Manager.css'
 function ManagerPortal(){
     return(
         <>
             <h1>Manager Portal</h1>
+            <div class = 'toprow'>
+                <button class = 'button' id = 'employeebtn'>Employees</button>
+                <button id = 'reportbtn'>Reports</button>
+            </div>
+            <div class = 'bottomrow'>
+                <button id = 'inventorybtn'>Inventory</button>
+                <button id = 'pricebtn'>Prices</button>
+            </div>
         </>
     )
     
