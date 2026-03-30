eb6bc019197e8c42bccd63eb113099cae591301f
Maher <23zavema@canacad.ac.jp>
2026-03-29 20:37:58 -0500

    Add SQL connection pool

---
+++ team_61_project3/db.js
@@ -0,0 +1,41 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  max: 10,
+  ssl: { rejectUnauthorized: false }
+});
+ 
+module.exports = pool;
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
