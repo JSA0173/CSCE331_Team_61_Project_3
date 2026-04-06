d87967e04bae34a62e738bdf956b4c6392d6a4f0
Eli Goodrich <erg2598@tamu.edu>
2026-04-06 13:23:42 -0500

    completed employee page

--- team_61_project3/src/views/Employee.jsx
+++ team_61_project3/src/views/Employee.jsx
@@ -26,16 +26,19 @@ function Employee({ setView }) {
                 <tbody>
                     {employees.map(emp => (
                         <tr key={emp.employeeId}>
-                            <td>{emp.employeeId}</td>
-                            <td>{emp.name}</td>
-                            <td>${parseFloat(emp.salary).toFixed(2)}</td>
-                            <td>{emp.job_title}</td>
-                            <td>{emp.date_hired ? emp.date_hired.split('T')[0] : ''}</td>
+                            <td class = 'odd'>{emp.employeeId}</td>
+                            <td class = 'even'>{emp.name}</td>
+                            <td class = 'odd'>${parseFloat(emp.salary).toFixed(2)}</td>
+                            <td class = 'even'>{emp.job_title}</td>
+                            <td class = 'odd'>{emp.date_hired ? emp.date_hired.split('T')[0] : ''}</td>
                         </tr>
                     ))}
                 </tbody>
             </table>
-            <button class = "employeebutton" onClick={() => setView('manager')}>Return to Manager Portal Home</button>
+            <button class = "employeebutton" id = 'back' onClick={() => setView('manager')}>Return to Manager Portal Home</button>
+            <button class = "employeebutton" id = 'addemp'>Add Employee</button>
+            <button class = "employeebutton" id = 'editemp'>Edit Employee</button>
+            <button class = "employeebutton" id = 'deleteemp'>Delete Employee</button>
         </>
     );
 }
