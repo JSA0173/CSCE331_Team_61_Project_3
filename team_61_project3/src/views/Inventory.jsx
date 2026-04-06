6bbce5668bc20d66fd2abb8378561cc648f938be
Maher <23zavema@canacad.ac.jp>
2026-04-06 13:31:02 -0500

    connection for inventory

--- team_61_project3/src/views/Inventory.jsx
+++ team_61_project3/src/views/Inventory.jsx
@@ -1,8 +1,47 @@
-function Inventory( {setView}){
-    return (<>
-        <h1>Inventory</h1>
-        <button class = 'button' id = 'managerbtn' onClick={() => setView('manager')}> Return to Manager Portal Home</button>
-    </>)
+import { useState, useEffect } from 'react';
+
+function Inventory({ setView }) {
+    const [inventory, setInventory] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/inventory')
+            .then(res => res.json())
+            .then(data => setInventory(data))
+            .catch(err => console.error(err));
+    }, []);
+
+    return (
+        <>
+            <h1>Inventory</h1>
+            <table className='data-table'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Quantity</th>
+                        <th>Price/Unit</th>
+                        <th>Reorder At</th>
+                        <th>Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {inventory.map(item => (
+                        <tr key={item.inventoryId}>
+                            <td>{item.inventoryId}</td>
+                            <td>{item.name}</td>
+                            <td>{item.quantityOnHand}</td>
+                            <td>${parseFloat(item.pricePerUnit).toFixed(2)}</td>
+                            <td>{item.reorderThreshold}</td>
+                            <td>{item.type}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <button className='button' id='managerbtn' onClick={() => setView('manager')}>
+                Return to Manager Portal Home
+            </button>
+        </>
+    );
 }
 
-export default Inventory
+export default Inventory;
