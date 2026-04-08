91f652d10196452cec77a28650e3789e6fecd5ee
Eli Goodrich <erg2598@tamu.edu>
2026-04-08 10:38:52 -0500

    added buttons to prices view

--- team_61_project3/src/views/Price.jsx
+++ team_61_project3/src/views/Price.jsx
@@ -1,7 +1,11 @@
+import './Price.jsx'
 function Price( {setView}){
     return (<>
         <h1>Prices</h1>
-        <button class = 'button' id = 'managerbtn' onClick={() => setView('manager')}> Return to Manager Portal Home</button>
+        <button class = 'employeebutton' onClick={() => setView('manager')}> Return to Manager Portal Home</button>
+        <button class = 'employeebutton'> Add Item</button>
+        <button class = 'employeebutton'> Update Price</button>
+        <button class = 'employeebutton'> Toggle Enabled</button>
     </>)
 }
 
