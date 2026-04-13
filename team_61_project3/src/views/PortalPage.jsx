98fe2062f3503c0ed05fa46803b871b3a9f70cf4
Grant Duong <grantd729@gmail.com>
2026-04-13 11:12:41 -0500

    portal page update

--- team_61_project3/src/views/PortalPage.jsx
+++ team_61_project3/src/views/PortalPage.jsx
@@ -1,14 +1,29 @@
 import './PortalPage.css';
 import GoogleTranslate from  './GoogleTranslate.jsx'
 
+
+
 function PortalPage( {setView}) {
+  const handleCashierClick = () => {
+    const enteredPin = prompt("Enter cashier PIN:");
+
+    if (!enteredPin) return;
+
+    const correctPin = import.meta.env.VITE_CASHIER_PASSWORD;
+
+    if (enteredPin === correctPin) {
+      setView('cashier');
+    } else {
+      alert("Incorrect PIN");
+    }
+  };
   return (
     <div class = 'text'>
       <h1>Boba Shop Home</h1>
       <h2>Select the desired portal using the buttons below:</h2>
       <div class = 'buttons'>
         <button class = 'button' id = 'managerbtn' onClick={() => setView('manager')}> Manager Portal</button>
-        <button class = 'button' id = 'cashierbtn' onClick={() => setView('cashier')}> Cashier Portal</button>
+        <button class = 'button' id = 'cashierbtn' onClick={handleCashierClick}> Cashier Portal</button>
         <button class = 'button' id = 'menubtn' onClick={() => setView('menu')}> Display Menu</button>
         <button class = 'button'id = 'kioskbtn' onClick={() => setView('kiosk')}> Customer Kiosk Portal</button>
       </div>
